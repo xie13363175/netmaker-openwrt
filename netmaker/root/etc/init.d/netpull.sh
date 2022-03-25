c250fa3fd5bbb17f8db7e88035d8f023386a606a
xcf13363175 <13363175@qq.com>
2022-03-25 10:30:24 +0800

    Delete netpull.sh

--- netmaker/root/etc/init.d/netpull.sh
+++
@@ -1,8 +0,0 @@
-        sleep 15
-        netok=$(ping 114.114.114.114 -c3 |grep "time=")
-        if [ "$netok" ]; then
-           logger Network OK
-           return
-        else
-           netclient pull
-        fi
