bfebfd584861f49aac401ebec6d571b4f02c2cda
xcf13363175 <13363175@qq.com>
2022-03-25 10:35:02 +0800

    Update netpull.sh

--- netpull.sh
+++ netpull.sh
@@ -1,5 +1,5 @@
         sleep 15
-        netok=$(ping 114.114.114.114 -c3 |grep "time=")
+        netok=$(ping 192.168.88.1 -c3 |grep "time=")
         if [ "$netok" ]; then
            logger Network OK
            return
